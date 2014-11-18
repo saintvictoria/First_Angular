ed259598f6be09fdb1e6304af163ba2cab3aba0f
Tim Whitacre <tim@timw.co>
2014-11-18 10:27:11 -0500

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+(function (){
+
+  angular.module('PeopleList', ['ngRoute'])
+
+  .config( function ($routeProvider) {
+
+    $routeProvider.when('/', {
+      templateUrl: 'templates/list-template.html',
+      controller: 'PersonController'
+    });
+
+    $routeProvider.when('/about', {
+      templateUrl: 'templates/about-template.html',
+      controller: 'AboutController'
+    });
+
+    $routeProvider.otherwise({
+      templateUrl: 'templates/other-template.html',
+      controller: 'OtherController'
+    });
+
+  });
+
+}());
+
+
+
+
+
+
+
